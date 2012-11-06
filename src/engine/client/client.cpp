42035b7d856f7dfbd229647725f5b851e1de9cfe
ftk <@>
2012-11-06 19:31:05 +0400

    disable console on win32

--- src/engine/client/client.cpp
+++ src/engine/client/client.cpp
@@ -2301,6 +2301,8 @@ int main(int argc, const char **argv) // ignore_convention
 {
 #endif
 #if defined(CONF_FAMILY_WINDOWS)
+	FreeConsole();
+	/*
 	for(int i = 1; i < argc; i++) // ignore_convention
 	{
 		if(str_comp("-s", argv[i]) == 0 || str_comp("--silent", argv[i]) == 0) // ignore_convention
@@ -2308,7 +2310,7 @@ int main(int argc, const char **argv) // ignore_convention
 			FreeConsole();
 			break;
 		}
-	}
+	}*/
 #endif
 
 	CClient *pClient = CreateClient();
