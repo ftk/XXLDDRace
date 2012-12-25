b83169e2243fec7c192501ea2c27aa19c939508e
ftk <@>
2012-12-25 22:30:14 +0400

    merge upstream

--- src/game/server/ddracechat.cpp
+++ src/game/server/ddracechat.cpp
@@ -1075,7 +1075,7 @@ void CGameContext::ConLogOut(IConsole::IResult *pResult, void *pUserData)
 	else
 		Victim = pResult->GetInteger(0);
 
-	if(!CheckClientID(Victim)) return;
+	if(!CheckRights(pResult->m_ClientID, Victim, (CGameContext *)pUserData)) return;
 
 	if (g_Config.m_SvRconScore)
 		pSelf->m_apPlayers[Victim]->m_Score = 0;
