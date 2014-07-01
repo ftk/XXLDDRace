7e86c999cd03621031f9e58c7b534f7dc296b3fa
ftk <@>
2014-07-01 16:13:03 +0400

    collision fix

--- src/game/collision.cpp
+++ src/game/collision.cpp
@@ -193,9 +193,9 @@ int CCollision::IntersectLine(vec2 Pos0, vec2 Pos1, vec2 *pOutCollision, vec2 *p
 		{
 			ThroughOffset(Pos0, Pos1, &dx, &dy);
 		}
-	for(int i = 0; i < End; i++)
+	for(int i = 0; i <= End; i++)
 	{
-		float a = i/Distance;
+		float a = i/(float)End;
 		vec2 Pos = mix(Pos0, Pos1, a);
 		ix = round(Pos.x);
 		iy = round(Pos.y);
